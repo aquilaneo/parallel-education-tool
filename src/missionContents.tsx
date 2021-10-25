c8eccf973f7c744131491aaf1ccb0ca2275828bf
aquilaneo <aquilaneo@gmail.com>
2021-10-25 22:26:03 +0900

    ミッション3-5を作成

--- src/missionContents.tsx
+++ src/missionContents.tsx
@@ -1117,7 +1117,55 @@ function isEqual (consoleOutputs: string, correctValue: number) {
 		judge: (consoleOutputs,
 				initialTwoDimensionalArrays, initialOneDimensionalArrays, initialVariables,
 				twoDimensionalArraysResult, oneDimensionalArraysResult, variablesResult) => {
-			if (variablesResult["sum"] === 0) {
+			if (variablesResult["Sum"] === 0) {
+				return {cleared: false, failReason: "計算結果が異なります。"};
+			}
+
+			return {cleared: true, failReason: ""};
+		}
+	});
+}
+
+// ミッション3-5
+{
+	const twoDimensionalArrays = new Mission.TwoDimensionalArrays ();
+	const oneDimensionalArrays = new Mission.OneDimensionalArrays ();
+	const globalVariables = new Mission.GlobalVariables ();
+	twoDimensionalArrays.addRandomArray ("Data", 4, 4, 1, 9);
+	globalVariables.addConstValue ("Sum", 0);
+	missionContents.addMissionContent ({
+		chapterName: "3章",
+		missionTitle: "[3章 並列プログラミング基礎編 - 5.排他制御2]",
+		missionExplanation: <div>ミューテックスを使うと複数スレッドから安全にグローバル領域に書き込みを行うことができます。</div>,
+		missionID: "mission3-05",
+		score: new Mission.MissionScore (),
+		program: "",
+		goal: <div>グローバル変数「sum」に配列「Data」の各要素の合計を並列処理で入れる。</div>,
+		blockList: {
+			behaviors: [BlockType.TEXT_PRINT, BlockType.WAIT_MS, BlockType.WAIT_S],
+			logic: [BlockType.CONTROLS_IF, BlockType.CONTROLS_IFELSE, BlockType.LOGIC_COMPARE, BlockType.LOGIC_OPERATION, BlockType.LOGIC_NEGATE],
+			loops: [BlockType.CONTROLS_REPEAT_EXT, BlockType.CONTROLS_WHILEUNTIL],
+			math: [BlockType.MATH_NUMBER, BlockType.MATH_ARITHMETIC],
+			text: [BlockType.TEXT, BlockType.STR_ARITHMETIC],
+			localVariables: [BlockType.LOCAL_VARIABLE_AVAILABLE],
+			globalArrays: [BlockType.GLOBAL_VARIABLE_READ, BlockType.GLOBAL_VARIABLE_WRITE, BlockType.GLOBAL_ONE_DIMENSIONAL_ARRAY_READ, BlockType.GLOBAL_ONE_DIMENSIONAL_ARRAY_WRITE, BlockType.GLOBAL_TWO_DIMENSIONAL_ARRAY_READ, BlockType.GLOBAL_TWO_DIMENSIONAL_ARRAY_WRITE],
+			functions: [BlockType.FUNCTION_DEFINITION, BlockType.FUNCTION_CALL, BlockType.ENTRY_POINT, BlockType.GET_ARGUMENT],
+			measurement: [BlockType.STOPWATCH_START, BlockType.STOPWATCH_STOP, BlockType.STOPWATCH_RESET, BlockType.STOPWATCH_READ],
+			parallel: [BlockType.THREAD_CREATE, BlockType.THREAD_JOIN, BlockType.MUTEX_LOCK, BlockType.MUTEX_UNLOCK]
+		},
+		twoDimensionalArrays: twoDimensionalArrays,
+		oneDimensionalArrays: oneDimensionalArrays,
+		globalVariables: globalVariables,
+		judge: (consoleOutputs,
+				initialTwoDimensionalArrays, initialOneDimensionalArrays, initialVariables,
+				twoDimensionalArraysResult, oneDimensionalArraysResult, variablesResult) => {
+			let sum = 0;
+			for (const row of initialTwoDimensionalArrays["Data"]) {
+				for (const col of row) {
+					sum += col;
+				}
+			}
+			if (variablesResult["Sum"] !== sum) {
 				return {cleared: false, failReason: "計算結果が異なります。"};
 			}
 
