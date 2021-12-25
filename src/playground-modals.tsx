20f6343053bc218a1293da4f957c59f01a8f5dde
aquilaneo <aquilaneo@gmail.com>
2021-12-25 14:37:16 +0900

    モーダルの背景を追加

--- src/playground-modals.tsx
+++ src/playground-modals.tsx
@@ -14,34 +14,36 @@ import BackIcon from "./img/Back.svg";
 // ミッション説明モーダル
 export const MissionDetailModal: React.FC<{ isVisible: boolean, setIsVisible: (value: boolean) => void, missionContent: MissionContent }> = (props) => {
 	return (
-		<div className={"modal-panel"} id={"mission-detail"} style={{display: props.isVisible ? "block" : "none"}}>
-			<div className={"modal-header"}>
-				<div className={"modal-header-chapter-name"}>{props.missionContent.chapterName}</div>
-				<div className={"modal-header-title"}>{props.missionContent.missionTitle}</div>
-			</div>
-			<div className={"modal-content"}>
-				<div className={"modal-item"}>
-					<div className={"modal-subtitle"}>
-						<div><img src={FlagIcon} className={"modal-icon"}/></div>
-						<div>クリア条件</div>
-					</div>
-					<div className={"center"}>{props.missionContent.goal}</div>
+		<div className={"modal-background"} style={{display: props.isVisible ? "block" : "none"}}>
+			<div className={"modal-panel"} id={"mission-detail"} style={{display: props.isVisible ? "block" : "none"}}>
+				<div className={"modal-header"}>
+					<div className={"modal-header-chapter-name"}>{props.missionContent.chapterName}</div>
+					<div className={"modal-header-title"}>{props.missionContent.missionTitle}</div>
 				</div>
-
-				<div className={"modal-item"}>
-					<div className={"modal-subtitle"}>
-						<div><img src={ExplanationIcon} className={"modal-icon"}/></div>
-						<div>ミッション説明</div>
+				<div className={"modal-content"}>
+					<div className={"modal-item"}>
+						<div className={"modal-subtitle"}>
+							<div><img src={FlagIcon} className={"modal-icon"}/></div>
+							<div>クリア条件</div>
+						</div>
+						<div className={"center"}>{props.missionContent.goal}</div>
 					</div>
-					<div>{props.missionContent.missionExplanation}</div>
-				</div>
 
-				<div className={"modal-item modal-buttons"}>
-					<div className={"modal-button"} onClick={() => {
-						props.setIsVisible (false);
-					}}>
-						<div><img src={CloseIcon} className={"modal-icon"}/></div>
-						<div className={"modal-button-text"}>閉じる</div>
+					<div className={"modal-item"}>
+						<div className={"modal-subtitle"}>
+							<div><img src={ExplanationIcon} className={"modal-icon"}/></div>
+							<div>ミッション説明</div>
+						</div>
+						<div>{props.missionContent.missionExplanation}</div>
+					</div>
+
+					<div className={"modal-item modal-buttons"}>
+						<div className={"modal-button"} onClick={() => {
+							props.setIsVisible (false);
+						}}>
+							<div><img src={CloseIcon} className={"modal-icon"}/></div>
+							<div className={"modal-button-text"}>閉じる</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -54,52 +56,54 @@ export const MissionClearModal: React.FC<{ isVisible: boolean, close: () => void
 	const chapter = props.nextMission ? props.nextMission.chapterNameURL : "";
 	const missionID = props.nextMission ? props.nextMission.missionID : "";
 	return (
-		<div className={"modal-panel"} id={"mission-clear"} style={{display: props.isVisible ? "block" : "none"}}>
-			<div className={"modal-header"}>
-				<div className={"modal-header-chapter-name"}>{props.missionContent.chapterName}</div>
-				<div className={"modal-header-title"}>{props.missionContent.missionTitle}</div>
-			</div>
-			<div className={"modal-content"}>
-				<div className={"modal-item modal-clear-text"}>MISSION CLEAR!</div>
-				<div className={"modal-item"}>
-					<div className={"modal-subtitle"}>
-						<div><img src={FlagIcon} className={"modal-icon"}/></div>
-						<div>クリア条件</div>
-					</div>
-					<div className={"center"}>{props.missionContent.goal}</div>
+		<div className={"modal-background"} style={{display: props.isVisible ? "block" : "none"}}>
+			<div className={"modal-panel"} id={"mission-clear"} style={{display: props.isVisible ? "block" : "none"}}>
+				<div className={"modal-header"}>
+					<div className={"modal-header-chapter-name"}>{props.missionContent.chapterName}</div>
+					<div className={"modal-header-title"}>{props.missionContent.missionTitle}</div>
 				</div>
+				<div className={"modal-content"}>
+					<div className={"modal-item modal-clear-text"}>MISSION CLEAR!</div>
+					<div className={"modal-item"}>
+						<div className={"modal-subtitle"}>
+							<div><img src={FlagIcon} className={"modal-icon"}/></div>
+							<div>クリア条件</div>
+						</div>
+						<div className={"center"}>{props.missionContent.goal}</div>
+					</div>
 
-				<div className={"modal-item"}>
-					<div className={"modal-subtitle"}>
-						<div><img src={ScoreIcon} className={"modal-icon"}/></div>
-						<div>スコア</div>
-					</div>
-					<div className={"score-row"}>
-						<div>実行時間:</div>
-						<div>{props.missionContent.score.getTimeSecond ()} 秒</div>
-					</div>
-					<div className={"score-row"}>
-						<div>ブロック数:</div>
-						<div>{props.missionContent.score.getBlockCount ()} 個</div>
-					</div>
-				</div>
-
-				<div className={"modal-item modal-buttons"}>
-					<div className={"modal-button"}>
-						<div><img src={BackIcon} className={"modal-icon"}/></div>
-						<div className={"modal-button-text"} onClick={() => {
-							props.close ();
-						}}>戻る
+					<div className={"modal-item"}>
+						<div className={"modal-subtitle"}>
+							<div><img src={ScoreIcon} className={"modal-icon"}/></div>
+							<div>スコア</div>
+						</div>
+						<div className={"score-row"}>
+							<div>実行時間:</div>
+							<div>{props.missionContent.score.getTimeSecond ()} 秒</div>
+						</div>
+						<div className={"score-row"}>
+							<div>ブロック数:</div>
+							<div>{props.missionContent.score.getBlockCount ()} 個</div>
 						</div>
 					</div>
-					<Link style={{display: props.nextMission ? "block" : "none"}} className={"modal-button-link"}
-						  to={`/missions/${chapter}/${missionID}`}>
+
+					<div className={"modal-item modal-buttons"}>
 						<div className={"modal-button"}>
-							<div><img src={NextIcon} className={"modal-icon"}/></div>
-							<div className={"modal-button-text"} id={"next-mission-button"}>次のミッションへ</div>
+							<div><img src={BackIcon} className={"modal-icon"}/></div>
+							<div className={"modal-button-text"} onClick={() => {
+								props.close ();
+							}}>戻る
+							</div>
 						</div>
-					</Link>
-					<div style={{display: props.nextMission ? "block" : "none"}} className={"modal-button-spacer"}/>
+						<Link style={{display: props.nextMission ? "block" : "none"}} className={"modal-button-link"}
+							  to={`/missions/${chapter}/${missionID}`}>
+							<div className={"modal-button"}>
+								<div><img src={NextIcon} className={"modal-icon"}/></div>
+								<div className={"modal-button-text"} id={"next-mission-button"}>次のミッションへ</div>
+							</div>
+						</Link>
+						<div style={{display: props.nextMission ? "block" : "none"}} className={"modal-button-spacer"}/>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -109,35 +113,37 @@ export const MissionClearModal: React.FC<{ isVisible: boolean, close: () => void
 // ミッション失敗モーダル
 export const MissionFailedModal: React.FC<{ isVisible: boolean, close: () => void, missionContent: MissionContent, failReason: string }> = (props) => {
 	return (
-		<div className={"modal-panel"} id={"mission-failed"} style={{display: props.isVisible ? "block" : "none"}}>
-			<div className={"modal-header"}>
-				<div className={"modal-header-chapter-name"}>{props.missionContent.chapterName}</div>
-				<div className={"modal-header-title"}>{props.missionContent.missionTitle}</div>
-			</div>
-			<div className={"modal-content"}>
-				<div className={"modal-item modal-failed-text"}>MISSION FAILED...</div>
-				<div className={"modal-item"}>
-					<div className={"modal-subtitle"}>
-						<div><img src={FlagIcon} className={"modal-icon"}/></div>
-						<div>クリア条件</div>
-					</div>
-					<div className={"center"}>{props.missionContent.goal}</div>
+		<div className={"modal-background"} style={{display: props.isVisible ? "block" : "none"}}>
+			<div className={"modal-panel"} id={"mission-failed"} style={{display: props.isVisible ? "block" : "none"}}>
+				<div className={"modal-header"}>
+					<div className={"modal-header-chapter-name"}>{props.missionContent.chapterName}</div>
+					<div className={"modal-header-title"}>{props.missionContent.missionTitle}</div>
 				</div>
-
-				<div className={"modal-item"}>
-					<div className={"modal-subtitle"}>
-						<div><img src={ExplanationIcon} className={"modal-icon"}/></div>
-						<div>失敗理由</div>
+				<div className={"modal-content"}>
+					<div className={"modal-item modal-failed-text"}>MISSION FAILED...</div>
+					<div className={"modal-item"}>
+						<div className={"modal-subtitle"}>
+							<div><img src={FlagIcon} className={"modal-icon"}/></div>
+							<div>クリア条件</div>
+						</div>
+						<div className={"center"}>{props.missionContent.goal}</div>
 					</div>
-					<div className={"center"}>{props.failReason}</div>
-				</div>
 
-				<div className={"modal-item modal-buttons"}>
-					<div className={"modal-button"}>
-						<div><img src={BackIcon} className={"modal-icon"}/></div>
-						<div className={"modal-button-text"} onClick={() => {
-							props.close ();
-						}}>ミッションに戻る
+					<div className={"modal-item"}>
+						<div className={"modal-subtitle"}>
+							<div><img src={ExplanationIcon} className={"modal-icon"}/></div>
+							<div>失敗理由</div>
+						</div>
+						<div className={"center"}>{props.failReason}</div>
+					</div>
+
+					<div className={"modal-item modal-buttons"}>
+						<div className={"modal-button"}>
+							<div><img src={BackIcon} className={"modal-icon"}/></div>
+							<div className={"modal-button-text"} onClick={() => {
+								props.close ();
+							}}>ミッションに戻る
+							</div>
 						</div>
 					</div>
 				</div>
